3e0bf3323c93390d2435d6ba52f8be9a14fad605
VBod <VBodalov@gmail.com>
2018-12-17 21:42:42 +0300

    Add axios

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
     //     ]
     // })
 
-
+    // const axios = require('axios');
 
     $('#userTable').bootstrapTable({
         url: '/user/find-all',
@@ -33,26 +33,36 @@ $(document).ready(function () {
             title: 'User password'
         }, {
             field: 'active',
-            title: 'Active'
-            // events: operateEvents
-            // formatter: operateFormatter
+            title: 'Active',
+            events: operateEvents,
+            formatter: operateFormatter
         }]
     });
-
-
-    window.operateEvents = {
-        'click .activeTogle': function (e, value, row, index) {
-            alert("fsdfsdf");
-        }
-    };
-
-
 });
+
+window.operateEvents = {
+    'click .activeToggle': function (e, value, row, index) {
+
+        axios.post('/user/toggleActive?userId=' + row.userId, {})
+            .then(function (response) {
+                $('#userTable').bootstrapTable(
+                    'updateCell', {
+                        index: index,
+                        field: "active",
+                        value: response
+                    });
+                alert(response);
+            })
+            .catch(function (error) {
+                alert(error);
+            });
+    }
+};
 
 function operateFormatter(value, row, index) {
     return [
-        '<a class="activeTogle" href="javascript:void(0)" title="Active togle">',
-        'active',
+        '<a class="activeToggle" href="javascript:void(0)" title="Active toggle">',
+        value,
         '</a>  '
     ].join('');
 }
