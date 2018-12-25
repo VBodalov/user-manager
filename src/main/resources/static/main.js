cfc981698a686d05ec1b07f2ba7d9b579ac0b04e
VBod <VBodalov@gmail.com>
2018-12-25 21:56:56 +0300

    WIP - adding dialog for editing credentials.

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -42,6 +42,36 @@ $(document).ready(function () {
                 alert("Error: " + JSON.stringify(error));
             });
     });
+
+    var name = $("#userNameDialog"),
+        password = $("#passwordDialog"),
+        allFields = $([]).add(userNameDialog).add(passwordDialog);
+
+    dialog = $("#edit-user-dialog").dialog({
+        autoOpen: false,
+        height: 400,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Create an account": addUser,
+            Cancel: function () {
+                dialog.dialog("close");
+            }
+        },
+        close: function () {
+            form[0].reset();
+            allFields.removeClass("ui-state-error");
+        }
+    });
+
+    form = dialog.find("form").on("submit", function (event) {
+        event.preventDefault();
+        addUser();
+    });
+
+    $("#create-user").button().on("click", function () {
+        dialog.dialog("open");
+    });
 });
 
 window.operateEvents = {
