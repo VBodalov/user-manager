8b3a8420f8634ec5bde13eec899b7c8021286abc
VBod <VBodalov@gmail.com>
2018-12-28 21:56:46 +0300

    WIP - adding edit credentials dialog.

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,24 +1,5 @@
 $(document).ready(function () {
 
-    $('#userTable').bootstrapTable({
-        url: '/user/find-all',
-        columns: [{
-            field: 'id',
-            title: 'User ID'
-        }, {
-            field: 'userName',
-            title: 'User name'
-        }, {
-            field: 'password',
-            title: 'Password'
-        }, {
-            field: 'active',
-            title: 'Active',
-            events: operateEvents,
-            formatter: operateFormatter
-        }]
-    });
-
     $("#addUserForm").submit(function (event) {
 
         event.preventDefault();
@@ -43,17 +24,43 @@ $(document).ready(function () {
             });
     });
 
-    var name = $("#userNameDialog"),
-        password = $("#passwordDialog"),
-        allFields = $([]).add(userNameDialog).add(passwordDialog);
+    $('#userTable').bootstrapTable({
+        url: '/user/find-all',
+        columns: [{
+            field: 'id',
+            title: 'User ID'
+        }, {
+            field: 'userName',
+            title: 'User name'
+        }, {
+            field: 'password',
+            title: 'Password'
+        }, {
+            field: 'active',
+            title: 'Active',
+            events: operateEvents,
+            formatter: operateFormatter
+        }],
+        onDblClickRow: function (row, $element, field) {
+            userId = (row.userId);
+            name.val(row.userName);
+            dialog.dialog("open");
+        }
+    });
 
-    dialog = $("#edit-user-dialog").dialog({
+    let userId;
+
+    var name = $("#editUserDialogName"),
+        password = $("#editUserDialogPassword"),
+        allFields = $([]).add(name).add(password);
+
+    const dialog = $("#edit-user-dialog").dialog({
         autoOpen: false,
         height: 400,
         width: 350,
         modal: true,
         buttons: {
-            "Create an account": addUser,
+            "Save": addUser,
             Cancel: function () {
                 dialog.dialog("close");
             }
@@ -64,12 +71,12 @@ $(document).ready(function () {
         }
     });
 
-    form = dialog.find("form").on("submit", function (event) {
+    const form = dialog.find("form").on("submit", function (event) {
         event.preventDefault();
-        addUser();
+        addUser(name.val(), password.val(), userId);
     });
 
-    $("#create-user").button().on("click", function () {
+    $("#edit-user").button().on("click", function () {
         dialog.dialog("open");
     });
 });
@@ -103,4 +110,29 @@ function operateFormatter(value, row, index) {
         value,
         '</a>  '
     ].join('');
+}
+
+function addUser(name, password, userId) {
+
+    alert(name);
+    alert(password);
+    alert(userId);
+
+    const userCredentials = {
+        userName: name,
+        password: password
+    };
+
+    $.ajax({
+        url: "/user/updateCredentials/" + userId,
+        method: "POST",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(userCredentials)
+    })
+        .done(function (response) {
+            $('#userTable').bootstrapTable('refresh');
+        })
+        .catch(function (error) {
+            alert("Error: " + JSON.stringify(error))
+        });
 }
